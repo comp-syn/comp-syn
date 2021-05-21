4d3fa8933da04c26b58c3e7c06c3b94b9fde082d
edarragh <edarragh@stanford.edu>
2021-05-21 00:06:07 -0700

    updated texture.py

--- compsyn/texture.py
+++ compsyn/texture.py
@@ -7,6 +7,25 @@ import torch
 L = 4
 J = 5
 
+
+def get_coefficents(L, J):
+	jj1 = []
+	index = []
+	jj2 = []
+	ll1 = []
+	ll2 = []
+	for j1 in range(J - 1):
+		for j2 in range(j1 + 1, J):
+			for l1 in range(L):
+				for l2 in range(L):
+					coeff_index = l1 * L * (J - j1 - 1) + l2 + L * (j2 - j1 - 1) + (L ** 2) * (j1 * (J - 1) - j1 * (j1 - 1) // 2)
+					index.append(coeff_index)
+					jj1.append(j1)
+					jj2.append(j2)
+					ll1.append(l1)
+					ll2.append(l2)
+	return np.array(index), np.array(jj1), np.array(jj2), np.array(ll1), np.array(ll2)
+
 def perform_scattering_transform(im):
 	'''Performs scattering transform on single channel  image array
 	In: im (numpy ndarray (128x128))
@@ -21,10 +40,29 @@ def perform_scattering_transform(im):
 	sc.append(scattering_coefficients[0])
 	sc1 = scattering_coefficients[1:J*L+1]
 	sc2 =  scattering_coefficients[J*L+1:]
+	coeffs, j1, j2, l1, l2 = get_coefficents(L, J)
+	a = []
+	b = []
+	c = []
+ 	sc_temp = []
 	for i in range(int(len(sc1)/L)):
 		sc.append(np.mean(sc1[L*i:L*(i+1)]))
 	for i in range(int(len(sc2)/L)):
-		sc.append(np.mean(sc2[L*i:L*(i+1)]))
+		sc_temp.append(np.mean(sc2[L*i:L*(i+1)]))
+		a.extend(np.unique(l1[index][i*L:(i+1)*L]))
+		b.extend(np.unique(j1[index][i*L:(i+1)*L]))
+		c.extend(np.unique(j2[index][i*L:(i+1)*L]))
+	a = np.array(a)
+	b = np.array(b)
+	c = np.array(c) 
+	sc_temp = np.array(sc_temp)
+	for i in (np.unique(b)):
+		for j in (np.unique(c)):
+			mask1 = b == i 
+			mask2 = c == j
+			mask = mask1&mask2
+			if(len(sc_temp[mask])>0):
+				sc.append(np.mean(sc_temp[mask]))
 	return sc 
 
 def get_wavelet_embedding(im, mode='JzAzBz'):
